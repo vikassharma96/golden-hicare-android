e6451c09cd4e2a00e877684532025af3ace420a6
vikassharma <teckudos@gmail.com>
2020-01-26 10:07:53 +0530

    initial commit

---
+++ app/src/main/java/com/teckudos/goldenhicare/views/SplashFragment.kt
@@ -0,0 +1,37 @@
+package com.teckudos.goldenhicare.views
+
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.teckudos.goldenhicare.R
+import com.teckudos.goldenhicare.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+
+    private var handler = Handler()
+    private lateinit var runnable: Runnable
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSplashBinding.inflate(inflater)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+        runnable = Runnable {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+}
